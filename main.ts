da99f2af6ec610518bb86b2dc5c09c15303a0e46
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-10 02:53:44 +0530

    get movies

--- main.ts
+++ main.ts
@@ -2,22 +2,28 @@
 import {Observable} from "rxjs-compat";
 
 let numbers=[1,5,10];
-let circle=document.getElementById('circle');
-let source =Observable.fromEvent(document,"mousemove")
-.map((e:MouseEvent)=>{
-    return {
-        x:e.clientX,
-        y:e.clientY
-    }
-})
-.filter(value=>value.x<500)
-.delay(300);
-function onNext(value){
-    circle.style.left=value.x;
-    circle.style.top=value.y;
+let output=document.getElementById('output');
+let button=document.getElementById('button');
+
+let click =Observable.fromEvent(button,"click")
+function load(url:string)
+{
+    let xhr=new XMLHttpRequest();
+    xhr.addEventListener("load",()=>{
+        let movies=JSON.parse(xhr.responseText);
+        movies.forEach(element => {
+            let div=document.createElement("div");
+            div.innerText=element.title;
+            output.appendChild(div);
+        });
+    });
+    xhr.open("GET",url);
+    xhr.send();
+
+
 }
-source.subscribe(
-onNext,  //this gets called first
+click.subscribe(
+e=>load("movies.json"),  //this gets called first
 e=>console.log(`error: ${e}`),  //next this
 ()=>console.log("complete") //lastly this
 )

aa2628c52c1f6b8428508e8378a82ca966be825b
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-10 03:33:56 +0530

    retry

--- main.ts
+++ main.ts
@@ -11,13 +11,18 @@ function load(url: string) { // this code does not execute until someone subscri
 
         let xhr = new XMLHttpRequest();
         xhr.addEventListener("load", () => {
+            if(xhr.status===200){
             let data = JSON.parse(xhr.responseText);
             observer.next(data);
             observer.complete();
+            }
+            else{
+                observer.error(xhr.statusText);
+            }
         });
         xhr.open("GET",url);
         xhr.send();
-    });
+    }).retry(3);// try thrice
     
 
 
@@ -27,11 +32,14 @@ function renderMovies(movies) {
         let div = document.createElement("div");
         div.innerText = m.title;
         output.appendChild(div);
+
     });// clear child data to not make it look stacked
+    // output.innerHTML=""; //clear all children
+    
 }
-load("movies.json");  //test to see if any data is returned 
+ //test to see if any data is returned 
 // click.flatMap(e=>load("movies.json")).subscribe(o=>console.log(o));
-click.flatMap(e=>load("movies.json")) //seperate function to load data 
+click.flatMap(e=>load("moviess.json")) //seperate function to load data 
 .subscribe(  // and adding a subscribe to dpecify what we have to do with loaded data
     renderMovies,  //in this case display movies
     e => console.log(`error: ${e}`),  //next this

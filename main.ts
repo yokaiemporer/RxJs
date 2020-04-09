9f615796f080875e9e8981de25b64383c688fdb9
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-10 02:15:17 +0530

    fun cirle

--- main.ts
+++ main.ts
@@ -1,48 +1,24 @@
 // alert("ok!!!!!!!!");
-import {Observable} from "rxjs-compat/observable";
-// import "rxjs/add/operator/map";
+import {Observable} from "rxjs-compat";
+
 let numbers=[1,5,10];
-// let source=Observable.from(numbers);
-
-
-let source=Observable.create(observer=>{
-    let index=0;
-    let produceValue=()=>{
-        observer.next(numbers[index++]);
-        if(index<numbers.length)
-        setTimeout(produceValue,2000);
-        else
-        observer.complete();
+let circle=document.getElementById('circle');
+let source =Observable.fromEvent(document,"mousemove")
+.map((e:MouseEvent)=>{
+    return {
+        x:e.clientX,
+        y:e.clientY
     }
-    produceValue();
-    // for(let n of numbers)  //value iteration -use let in numbers for index iteration
-    // {
-    //     if(n==5)
-    //     observer.error("something went wrong!");//stop the observable with this function
-    //     observer.next(n);
-    // }
-    // observer.complete();
-}).map(n=>n*2).filter(n=>n>4);
+})
+.filter(value=>value.x<500)
+.delay(300);
+function onNext(value){
+    circle.style.left=value.x;
+    circle.style.top=value.y;
+}
 source.subscribe(
-value=>console.log(`value: ${value}`),  //this gets called first
+onNext,  //this gets called first
 e=>console.log(`error: ${e}`),  //next this
 ()=>console.log("complete") //lastly this
 )
-// class MyObserver
-// {
 
-//     next(value)
-//     {
-//         console.log(`value: ${value}`);
-//     }
-//     error(e)
-//     {
-//         console.log(`error: ${e}`);
-
-//     }
-//     complete()
-//     {
-//         console.log("complete");
-//     }
-// }
-// source.subscribe(new MyObserver());

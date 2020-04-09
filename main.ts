5f1a32a8e3d9f82250520934f7b274c1986d1988
YokaiEmporer <taranrishit1234@gmail.com>
2020-04-10 03:05:08 +0530

    before using flat map for nested subscribes

--- main.ts
+++ main.ts
@@ -1,30 +1,40 @@
 // alert("ok!!!!!!!!");
-import {Observable} from "rxjs-compat";
+import { Observable } from "rxjs-compat";
 
-let numbers=[1,5,10];
-let output=document.getElementById('output');
-let button=document.getElementById('button');
+let numbers = [1, 5, 10];
+let output = document.getElementById('output');
+let button = document.getElementById('button');
 
-let click =Observable.fromEvent(button,"click")
-function load(url:string)
-{
-    let xhr=new XMLHttpRequest();
-    xhr.addEventListener("load",()=>{
-        let movies=JSON.parse(xhr.responseText);
-        movies.forEach(element => {
-            let div=document.createElement("div");
-            div.innerText=element.title;
-            output.appendChild(div);
+let click = Observable.fromEvent(button, "click")
+function load(url: string) {
+    return Observable.create(observer => {
+
+        let xhr = new XMLHttpRequest();
+        xhr.addEventListener("load", () => {
+            let data = JSON.parse(xhr.responseText);
+            observer.next(data);
+            observer.complete();
         });
+        xhr.open("GET",url);
+        xhr.send();
     });
-    xhr.open("GET",url);
-    xhr.send();
+    
 
 
 }
+function renderMovies(movies) {
+    movies.forEach(m => {
+        let div = document.createElement("div");
+        div.innerText = m.title;
+        output.appendChild(div);
+    });
+}
+//same thing bfore flat map
 click.subscribe(
-e=>load("movies.json"),  //this gets called first
-e=>console.log(`error: ${e}`),  //next this
-()=>console.log("complete") //lastly this
+    e => load("movies.json").subscribe(o=>console.log(o)),  //this gets called first
+    e => console.log(`error: ${e}`),  //next this
+    () => console.log("complete") //lastly this
 )
+//there is a nesting of scubscribe inside one another 
+//so we can better implment this in next commit
 
